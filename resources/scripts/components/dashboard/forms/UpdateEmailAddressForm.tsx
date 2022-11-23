0a3557337ef778f6af87927fcb245b7adcdc72e3
Drylian <109999325+drylian@users.noreply.github.com>
2022-11-22 22:38:47 -0300

    dashboard

---
+++ resources/scripts/components/dashboard/forms/UpdateEmailAddressForm.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import * as Yup from 'yup';
+import tw from 'twin.macro';
+import { ApplicationStore } from '@/state';
+import { httpErrorToHuman } from '@/api/http';
+import Field from '@/components/elements/Field';
+import { Form, Formik, FormikHelpers } from 'formik';
+import { Button } from '@/components/elements/button/index';
+import SpinnerOverlay from '@/components/elements/SpinnerOverlay';
+import { Actions, State, useStoreActions, useStoreState } from 'easy-peasy';
+
+interface Values {
+    email: string;
+    password: string;
+}
+
+const schema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().required('Deve fornecer a sua senha de conta corrente.'),
+});
+
+export default () => {
+    const user = useStoreState((state: State<ApplicationStore>) => state.user.data);
+    const updateEmail = useStoreActions((state: Actions<ApplicationStore>) => state.user.updateUserEmail);
+
+    const { clearFlashes, addFlash } = useStoreActions((actions: Actions<ApplicationStore>) => actions.flashes);
+
+    const submit = (values: Values, { resetForm, setSubmitting }: FormikHelpers<Values>) => {
+        clearFlashes('account:email');
+
+        updateEmail({ ...values })
+            .then(() =>
+                addFlash({
+                    type: 'success',
+                    key: 'account:email',
+                    message: 'O seu e-mail principal foi atualizado.',
+                })
+            )
+            .catch((error) =>
+                addFlash({
+                    type: 'error',
+                    key: 'account:email',
+                    title: 'Erro',
+                    message: httpErrorToHuman(error),
+                })
+            )
+            .then(() => {
+                resetForm();
+                setSubmitting(false);
+            });
+    };
+
+    return (
+        <Formik onSubmit={submit} validationSchema={schema} initialValues={{ email: user!.email, password: '' }}>
+            {({ isSubmitting, isValid }) => (
+                <React.Fragment>
+                    <SpinnerOverlay size={'large'} visible={isSubmitting} />
+                    <Form css={tw`m-0`}>
+                        <Field id={'current_email'} type={'email'} name={'email'} label={'Email'} />
+                        <div css={tw`mt-6`}>
+                            <Field
+                                id={'confirm_password'}
+                                type={'password'}
+                                name={'password'}
+                                label={'Confirmar Senha'}
+                            />
+                        </div>
+                        <div css={tw`mt-6`}>
+                            <Button disabled={isSubmitting || !isValid}>Atualizar Email</Button>
+                        </div>
+                    </Form>
+                </React.Fragment>
+            )}
+        </Formik>
+    );
+};
---
+++ resources/scripts/components/dashboard/forms/UpdatePasswordForm.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import * as Yup from 'yup';
+import tw from 'twin.macro';
+import { ApplicationStore } from '@/state';
+import { httpErrorToHuman } from '@/api/http';
+import Field from '@/components/elements/Field';
+import { Form, Formik, FormikHelpers } from 'formik';
+import { Button } from '@/components/elements/button/index';
+import SpinnerOverlay from '@/components/elements/SpinnerOverlay';
+import updateAccountPassword from '@/api/account/updateAccountPassword';
+import { Actions, State, useStoreActions, useStoreState } from 'easy-peasy';
+
+interface Values {
+    current: string;
+    password: string;
+    confirmPassword: string;
+}
+
+const schema = Yup.object().shape({
+    current: Yup.string().min(1).required('Deve fornecer a sua palavra-passe actual.'),
+    password: Yup.string().min(8).required(),
+    confirmPassword: Yup.string().test(
+        'password',
+        'A confirmação da senha não corresponde à senha que introduziu.',
+        function (value) {
+            return value === this.parent.password;
+        }
+    ),
+});
+
+export default () => {
+    const user = useStoreState((state: State<ApplicationStore>) => state.user.data);
+    const { clearFlashes, addFlash } = useStoreActions((actions: Actions<ApplicationStore>) => actions.flashes);
+
+    if (!user) {
+        return null;
+    }
+
+    const submit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+        clearFlashes('account:password');
+        updateAccountPassword({ ...values })
+            .then(() => {
+                // @ts-expect-error this is valid
+                window.location = '/auth/login';
+            })
+            .catch((error) =>
+                addFlash({
+                    key: 'account:password',
+                    type: 'error',
+                    title: 'Erro',
+                    message: httpErrorToHuman(error),
+                })
+            )
+            .then(() => setSubmitting(false));
+    };
+
+    return (
+        <React.Fragment>
+            <Formik
+                onSubmit={submit}
+                validationSchema={schema}
+                initialValues={{ current: '', password: '', confirmPassword: '' }}
+            >
+                {({ isSubmitting, isValid }) => (
+                    <React.Fragment>
+                        <SpinnerOverlay size={'large'} visible={isSubmitting} />
+                        <Form css={tw`m-0`}>
+                            <Field
+                                id={'current_password'}
+                                type={'password'}
+                                name={'current'}
+                                label={'Senha Atual'}
+                            />
+                            <div css={tw`mt-6`}>
+                                <Field
+                                    id={'new_password'}
+                                    type={'password'}
+                                    name={'password'}
+                                    label={'Nova Senha'}
+                                    description={
+                                        'A sua nova senha deve ter pelo menos 8 caracteres e ser única para este WebSite.'
+                                    }
+                                />
+                            </div>
+                            <div css={tw`mt-6`}>
+                                <Field
+                                    id={'confirm_new_password'}
+                                    type={'password'}
+                                    name={'confirmPassword'}
+                                    label={'Confirmar Nova Senha'}
+                                />
+                            </div>
+                            <div css={tw`mt-6`}>
+                                <Button disabled={isSubmitting || !isValid}>Atualizar Senha</Button>
+                            </div>
+                        </Form>
+                    </React.Fragment>
+                )}
+            </Formik>
+        </React.Fragment>
+    );
+};

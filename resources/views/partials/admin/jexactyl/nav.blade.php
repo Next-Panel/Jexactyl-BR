accf55193aee35ab62bb0dcef828709193b77323
Drylian <109999325+drylian@users.noreply.github.com>
2022-11-23 01:33:18 -0300

    TRADUÇÃO PT-BR
    Tradução do menu jexactyl

--- resources/views/partials/admin/jexactyl/nav.blade.php
+++ resources/views/partials/admin/jexactyl/nav.blade.php
@@ -3,16 +3,16 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'index')class="active"@endif><a href="{{ route('admin.index') }}">Home</a></li>
-                    <li @if($activeTab === 'theme')class="active"@endif><a href="{{ route('admin.jexactyl.theme') }}">Appearance</a></li>
-                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.jexactyl.mail') }}">Mail</a></li>
-                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.jexactyl.advanced') }}">Advanced</a></li>
+                    <li @if($activeTab === 'index')class="active"@endif><a href="{{ route('admin.index') }}">Casa</a></li>
+                    <li @if($activeTab === 'theme')class="active"@endif><a href="{{ route('admin.jexactyl.theme') }}">Apar&ecirc;ncia</a></li>
+                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.jexactyl.mail') }}">E-Mail</a></li>
+                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.jexactyl.advanced') }}">Advan&ccedil;ado</a></li>
                     <li style="margin-left: 5px; margin-right: 5px;"><a>-</a></li>
-                    <li @if($activeTab === 'store')class="active"@endif><a href="{{ route('admin.jexactyl.store') }}">Storefront</a></li>
-                    <li @if($activeTab === 'registration')class="active"@endif><a href="{{ route('admin.jexactyl.registration') }}">Registration</a></li>
-                    <li @if($activeTab === 'approvals')class="active"@endif><a href="{{ route('admin.jexactyl.approvals') }}">Approvals</a></li>
-                    <li @if($activeTab === 'server')class="active"@endif><a href="{{ route('admin.jexactyl.server') }}">Server Settings</a></li>
-                    <li @if($activeTab === 'referrals')class="active"@endif><a href="{{ route('admin.jexactyl.referrals') }}">Referrals</a></li>
+                    <li @if($activeTab === 'store')class="active"@endif><a href="{{ route('admin.jexactyl.store') }}">Loja</a></li>
+                    <li @if($activeTab === 'registration')class="active"@endif><a href="{{ route('admin.jexactyl.registration') }}">Registra&ccedil;&atilde;o</a></li>
+                    <li @if($activeTab === 'approvals')class="active"@endif><a href="{{ route('admin.jexactyl.approvals') }}">Aprova&ccedil;&otilde;es</a></li>
+                    <li @if($activeTab === 'server')class="active"@endif><a href="{{ route('admin.jexactyl.server') }}">Configurar Servidores</a></li>
+                    <li @if($activeTab === 'referrals')class="active"@endif><a href="{{ route('admin.jexactyl.referrals') }}">Refer&eacute;ncias</a></li>
                     
                 </ul>
             </div>

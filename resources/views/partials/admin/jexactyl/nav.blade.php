8ad61a0cb41e860d8b5ba1ee6da3d5eada3f8bf1
Matheus Nilton <matheusn.biolowons@gmail.com>
2022-11-24 14:24:55 -0400

    Correções de traduções

--- resources/views/partials/admin/jexactyl/nav.blade.php
+++ resources/views/partials/admin/jexactyl/nav.blade.php
@@ -3,10 +3,10 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'index')class="active"@endif><a href="{{ route('admin.index') }}">Casa</a></li>
+                    <li @if($activeTab === 'index')class="active"@endif><a href="{{ route('admin.index') }}">Home</a></li>
                     <li @if($activeTab === 'theme')class="active"@endif><a href="{{ route('admin.jexactyl.theme') }}">Apar&ecirc;ncia</a></li>
                     <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.jexactyl.mail') }}">E-Mail</a></li>
-                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.jexactyl.advanced') }}">Advan&ccedil;ado</a></li>
+                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.jexactyl.advanced') }}">Avançado</a></li>
                     <li style="margin-left: 5px; margin-right: 5px;"><a>-</a></li>
                     <li @if($activeTab === 'store')class="active"@endif><a href="{{ route('admin.jexactyl.store') }}">Loja</a></li>
                     <li @if($activeTab === 'registration')class="active"@endif><a href="{{ route('admin.jexactyl.registration') }}">Registra&ccedil;&atilde;o</a></li>

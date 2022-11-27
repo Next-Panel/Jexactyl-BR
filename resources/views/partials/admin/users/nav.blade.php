f1898d2b69ef521eaea7400189990c6c01e6b3f8
Matheus Nilton <matheusn.biolowons@gmail.com>
2022-11-27 00:05:23 -0400

    Correção critico

--- resources/views/partials/admin/users/nav.blade.php
+++ resources/views/partials/admin/users/nav.blade.php
@@ -3,8 +3,8 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'overview')class="active"@endif><a href="{{ route('admin.users.view', ['user' => $user]) }}">Vis&atilde;o Geral</a></li>
-                    <li @if($activeTab === 'storefront')class="active"@endif><a href="{{ route('admin.users.store', ['user' => $user]) }}">Recursos</a></li>
+                    <li @if($activeTab === 'overview')class="active"@endif><a href="{{ route('admin.users.view', ['user' => $user]) }}">Visão geral</a></li>
+                    <li @if($activeTab === 'resources')class="active"@endif><a href="{{ route('admin.users.resources', ['user' => $user]) }}">Recursos</a></li>
                 </ul>
             </div>
         </div>

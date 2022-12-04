437a8a69373b331cded4395fc4809df788497b80
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-04 18:22:22 -0400

    Update SendPasswordReset.php

--- app/Notifications/SendPasswordReset.php
+++ app/Notifications/SendPasswordReset.php
@@ -32,9 +32,9 @@ class SendPasswordReset extends Notification implements ShouldQueue
     public function toMail(mixed $notifiable): MailMessage
     {
         return (new MailMessage())
-            ->subject('Reset Password')
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url('/auth/password/reset/' . $this->token . '?email=' . urlencode($notifiable->email)))
-            ->line('If you did not request a password reset, no further action is required.');
+            ->subject('Redefinir senha')
+            ->line('Você está recebendo este e-mail porque recebemos uma solicitação de redefinição de senha para sua conta.')
+            ->action('RRedefinir senha', url('/auth/password/reset/' . $this->token . '?email=' . urlencode($notifiable->email)))
+            ->line('Se você não solicitou uma redefinição de senha, nenhuma outra ação é necessária.');
     }
 }

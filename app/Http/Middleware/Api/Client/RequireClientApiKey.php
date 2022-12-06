360428743a4bdb9d3b87fed5a4fa1377acfda2e1
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 14:23:50 -0400

    Update RequireClientApiKey.php

--- app/Http/Middleware/Api/Client/RequireClientApiKey.php
+++ app/Http/Middleware/Api/Client/RequireClientApiKey.php
@@ -17,7 +17,7 @@ class RequireClientApiKey
         $token = $request->user()->currentAccessToken();
 
         if ($token instanceof ApiKey && $token->key_type === ApiKey::TYPE_APPLICATION) {
-            throw new AccessDeniedHttpException('You are attempting to use an application API key on an endpoint that requires a client API key.');
+            throw new AccessDeniedHttpException('Você está tentando usar uma chave de API do aplicativo em um terminal que requer uma chave de API do cliente.');
         }
 
         return $next($request);

497445ccaa9d1abf63bc43fe8b2424d0700a6db2
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 17:06:33 -0400

    Update Fqdn.php

--- app/Rules/Fqdn.php
+++ app/Rules/Fqdn.php
@@ -38,7 +38,7 @@ class Fqdn implements Rule, DataAwareRule
             // custom SSL cert, IPs will not be able to use HTTPS.  This should prevent most
             // home users from making this mistake and wondering why their node is not working.
             if ($this->schemeField && Arr::get($this->data, $this->schemeField) === 'https') {
-                $this->message = 'The :attribute must not be an IP address when HTTPS is enabled.';
+                $this->message = 'O :attribute não deve ser um endereço IP quando o HTTPS está ativado.';
 
                 return false;
             }
@@ -58,7 +58,7 @@ class Fqdn implements Rule, DataAwareRule
             return true;
         }
 
-        $this->message = 'The :attribute could not be resolved to a valid IP address.';
+        $this->message = 'O :attribute não pôde ser resolvido para um endereço IP válido.';
 
         return false;
     }

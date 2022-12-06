b813789b04b19c9fd782cd37b9ce5611f9a34d87
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-05 23:50:57 -0400

    dialog

--- app/Http/Controllers/Admin/Users/ResourceController.php
+++ app/Http/Controllers/Admin/Users/ResourceController.php
@@ -41,7 +41,7 @@ class ResourceController extends Controller
             ->setUserLevel(User::USER_LEVEL_ADMIN)
             ->handle($user, $request->normalize());
 
-        $this->alert->success('User resources have been updated.')->flash();
+        $this->alert->success('Os recursos do usuário foram atualizados.')->flash();
 
         return redirect()->route('admin.users.resources', $user->id);
     }

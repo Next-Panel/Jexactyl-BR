a315e88809b0ae2a42977b6791e2962032c28ef5
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 16:46:19 -0400

    Update MakeUserCommand.php

--- app/Console/Commands/User/MakeUserCommand.php
+++ app/Console/Commands/User/MakeUserCommand.php
@@ -7,7 +7,7 @@ use Pterodactyl\Services\Users\UserCreationService;
 
 class MakeUserCommand extends Command
 {
-    protected $description = 'Creates a user on the system via the CLI.';
+    protected $description = 'Cria um usuário no sistema via CLI.';
 
     protected $signature = 'p:user:make {--email=} {--username=} {--name-first=} {--name-last=} {--password=} {--admin=} {--no-password}';
 

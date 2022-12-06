aefa0397d4912e1ccbd168a5060e3153fb274a6b
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 14:25:23 -0400

    Update DaemonAuthenticate.php

--- app/Http/Middleware/Api/Daemon/DaemonAuthenticate.php
+++ app/Http/Middleware/Api/Daemon/DaemonAuthenticate.php
@@ -39,13 +39,13 @@ class DaemonAuthenticate
         }
 
         if (is_null($bearer = $request->bearerToken())) {
-            throw new HttpException(401, 'Access to this endpoint must include an Authorization header.', null, ['WWW-Authenticate' => 'Bearer']);
+            throw new HttpException(401, 'O acesso a este endpoint deve incluir um cabeçalho de autorização.', null, ['WWW-Authenticate' => 'Bearer']);
         }
 
         $parts = explode('.', $bearer);
         // Ensure that all of the correct parts are provided in the header.
         if (count($parts) !== 2 || empty($parts[0]) || empty($parts[1])) {
-            throw new BadRequestHttpException('The Authorization header provided was not in a valid format.');
+            throw new BadRequestHttpException('O cabeçalho de autorização fornecido não estava em um formato válido.');
         }
 
         try {
@@ -63,6 +63,6 @@ class DaemonAuthenticate
             // Do nothing, we don't want to expose a node not existing at all.
         }
 
-        throw new AccessDeniedHttpException('You are not authorized to access this resource.');
+        throw new AccessDeniedHttpException('Você não está autorizado a acessar este recurso.');
     }
 }

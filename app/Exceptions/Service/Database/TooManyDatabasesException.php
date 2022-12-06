e272fd04169c6cdc3c2b10e2502f8554b1c7836e
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 19:21:17 -0400

    Update TooManyDatabasesException.php

--- app/Exceptions/Service/Database/TooManyDatabasesException.php
+++ app/Exceptions/Service/Database/TooManyDatabasesException.php
@@ -8,6 +8,6 @@ class TooManyDatabasesException extends DisplayException
 {
     public function __construct()
     {
-        parent::__construct('Operation aborted: creating a new database would put this server over the defined limit.');
+        parent::__construct('Operação abortada: a criação de um novo banco de dados colocaria este servidor acima do limite definido.');
     }
 }

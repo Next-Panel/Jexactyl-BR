89af61edb38f0231f4ca1a09ffd5298eb9e5a121
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 19:14:53 -0400

    Update TwoFactorAuthenticationTokenInvalid.php

--- app/Exceptions/Service/User/TwoFactorAuthenticationTokenInvalid.php
+++ app/Exceptions/Service/User/TwoFactorAuthenticationTokenInvalid.php
@@ -11,6 +11,6 @@ class TwoFactorAuthenticationTokenInvalid extends DisplayException
      */
     public function __construct()
     {
-        parent::__construct('The provided two-factor authentication token was not valid.');
+        parent::__construct('O token de autenticação de dois fatores fornecido não era válido');
     }
 }

f6b8cf4a8e10fc1af2142820fbf68082fdd7b8d3
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-05 23:58:08 -0400

    Update LocationController.php

--- app/Http/Controllers/Admin/LocationController.php
+++ app/Http/Controllers/Admin/LocationController.php
@@ -60,7 +60,7 @@ class LocationController extends Controller
     public function create(LocationFormRequest $request): RedirectResponse
     {
         $location = $this->creationService->handle($request->normalize());
-        $this->alert->success('Location was created successfully.')->flash();
+        $this->alert->success('O local foi criado com sucesso.')->flash();
 
         return redirect()->route('admin.locations.view', $location->id);
     }
@@ -77,7 +77,7 @@ class LocationController extends Controller
         }
 
         $this->updateService->handle($location->id, $request->normalize());
-        $this->alert->success('Location was updated successfully.')->flash();
+        $this->alert->success('A localização foi atualizada com sucesso.')->flash();
 
         return redirect()->route('admin.locations.view', $location->id);
     }

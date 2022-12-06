5902bc5c6f5541f8a0f24bbcc32d22c3e8e7a624
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-06 19:10:54 -0400

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -200,14 +200,14 @@ class Handler extends ExceptionHandler
                 : strval($match ?? '500'),
             'detail' => $e instanceof HttpExceptionInterface || !is_null($match)
                 ? $e->getMessage()
-                : 'An unexpected error was encountered while processing this request, please try again.',
+                : 'Um erro inesperado foi encontrado ao processar esta solicitação, tente novamente.',
         ];
 
         if ($e instanceof ModelNotFoundException || $e->getPrevious() instanceof ModelNotFoundException) {
             // Show a nicer error message compared to the standard "No query results for model"
             // response that is normally returned. If we are in debug mode this will get overwritten
             // with a more specific error message to help narrow down things.
-            $error['detail'] = 'The requested resource could not be found on the server.';
+            $error['detail'] = 'O recurso solicitado não foi encontrado no servidor.';
         }
 
         if (config('app.debug')) {

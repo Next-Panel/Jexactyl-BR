413401c58bef8f06c89418b1877cab3260f724d5
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-08 18:54:25 -0400

    ada....

--- tests/Integration/Api/Client/Server/Startup/UpdateStartupVariableTest.php
+++ tests/Integration/Api/Client/Server/Startup/UpdateStartupVariableTest.php
@@ -30,7 +30,7 @@ class UpdateStartupVariableTest extends ClientApiIntegrationTestCase
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
         $response->assertJsonPath('errors.0.code', 'ValidationException');
-        $response->assertJsonPath('errors.0.detail', 'The value may only contain letters and numbers.');
+        $response->assertJsonPath('errors.0.detail', 'O value só pode conter letras e números.');
 
         $response = $this->actingAs($user)->putJson($this->link($server) . '/startup/variable', [
             'key' => 'BUNGEE_VERSION',

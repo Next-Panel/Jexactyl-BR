e9b2f94a5019eb69bd8619a31da0e8718c9df2e6
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-15 16:15:47 -0400

    passou despercebido

--- resources/views/admin/servers/index.blade.php
+++ resources/views/admin/servers/index.blade.php
@@ -21,7 +21,7 @@ Lista de servidores
                 <div class="box-tools search01">
                     <form action="{{ route('admin.servers') }}" method="GET">
                         <div class="input-group input-group-sm">
-                            <input type="text" name="filter[*]" class="form-control pull-right" value="{{ request()->input()['filter']['*'] ?? '' }}" placeholder="Search Servers">
+                            <input type="text" name="filter[*]" class="form-control pull-right" value="{{ request()->input()['filter']['*'] ?? '' }}" placeholder="Procurar Servidores">
                             <div class="input-group-btn">
                                 <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                                 <a href="{{ route('admin.servers.new') }}"><button type="button" class="btn btn-sm btn-primary" style="border-radius: 0 3px 3px 0;margin-left:-1px;">Criar Novo</button></a>

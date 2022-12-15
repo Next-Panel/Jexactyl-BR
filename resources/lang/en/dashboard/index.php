be78b2ba0562dff70561f498ddc3d6fc8e76f9c3
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-14 23:35:12 -0400

    Delete resources/lang/en directory
    pro painel n selecionar ela por padrao

--- resources/lang/en/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Busca por servidores...',
-    'no_matches' => 'Não foram encontrados servidores que correspondem aos critérios de pesquisa fornecidos.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memoria',
-];

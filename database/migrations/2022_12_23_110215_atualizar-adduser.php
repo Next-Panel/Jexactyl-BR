7bb137df5a9e570cd89fd67c9e7c4ed9be3deed1
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-23 14:09:49 -0400

    teste

---
+++ database/migrations/2022_12_23_110215_atualizar-adduser.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAddUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('uuid', 36)->unique();
+            $table->string('email')->unique();
+            $table->text('password');
+            $table->string('remember_token')->nullable();
+            $table->char('language', 5)->default('pt');
+            $table->tinyInteger('root_admin')->unsigned()->default(0);
+            $table->tinyInteger('use_totp')->unsigned();
+            $table->char('totp_secret', 16)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        {
+            Schema::table('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->char('uuid', 36)->unique();
+                $table->string('email')->unique();
+                $table->text('password');
+                $table->string('remember_token')->nullable();
+                $table->char('language', 5)->default('en');
+                $table->tinyInteger('root_admin')->unsigned()->default(0);
+                $table->tinyInteger('use_totp')->unsigned();
+                $table->char('totp_secret', 16)->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+}

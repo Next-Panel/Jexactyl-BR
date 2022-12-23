001e0cf87d774802e6340974d50b8675522abee7
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-23 14:42:38 -0400

    Create 2022_12_23_110215_atualizar_adduser.php

--- database/migrations/2022_12_23_110215_atualizar_adduser.php
+++ database/migrations/2022_12_23_110215_atualizar_adduser.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AtualizarAddUsers extends Migration
+class AddUsers extends Migration
 {
     /**
      * Run the migrations.

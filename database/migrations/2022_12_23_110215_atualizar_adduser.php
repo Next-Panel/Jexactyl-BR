8e55f6a2a7a6c0aa21e1ae939644137d007c52f2
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-23 14:15:53 -0400

    teste?

--- database/migrations/2022_12_23_110215_atualizar_adduser.php
+++ database/migrations/2022_12_23_110215_atualizar_adduser.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateAddUsers extends Migration
+class AtualizarAddUsers extends Migration
 {
     /**
      * Run the migrations.

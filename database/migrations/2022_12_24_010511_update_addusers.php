5c64763aec4f18e0c79e43762766e8b02499df02
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-24 02:24:57 -0400

    um espaço literalmente, um espaço 7-7

--- database/migrations/2022_12_24_010511_update_addusers.php
+++ database/migrations/2022_12_24_010511_update_addusers.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 return new class () extends Migration {
     /**
      * Run the migrations.
-     * 
+     *
      * @return void
      */
     public function up()

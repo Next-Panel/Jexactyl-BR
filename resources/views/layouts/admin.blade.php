7240db9b189a8bfa0430737295b0d6e208b189a6
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-13 02:51:37 -0400

    Agr provavelmente ta funcionando

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,6 +24,7 @@
             {!! Theme::css('vendor/bootstrap/bootstrap.min.css?t={cache-version}') !!}
             {!! Theme::css('vendor/adminlte/admin.min.css?t={cache-version}') !!}
             <!-- Habilita o sistema Sidebar da Jexactyl-Brasil -->
+            {!! Theme::css('vendor/sideb/sidebar.css?t={cache-version}')
             <link rel="stylesheet" href="/themes/default/vendor/{{ config('sidebar.tema', 'sidejx') }}/sidebar.css">
             {!! Theme::css('vendor/adminlte/colors/skin-blue.min.css?t={cache-version}') !!}
             {!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}

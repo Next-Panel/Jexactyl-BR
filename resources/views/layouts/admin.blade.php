a7d594b4826d8926d432b6f879298cc8a93c42b0
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-12 22:35:49 -0400

    Sitebar autônoma

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,32 +20,20 @@
         @include('layouts.scripts')
 
         @section('scripts')
-            <!-- Sistema Antigo de cores ------------------------------------------------------------------------------------------------------->
-            <!--{!! Theme::css('vendor/select2/select2.min.css?t={cache-version}') !!}---------------------------------------------------------->
-            <!--{!! Theme::css('vendor/bootstrap/bootstrap.min.css?t={cache-version}') !!}------------------------------------------------------>
-            <!--{!! Theme::css('vendor/adminlte/admin.min.css?t={cache-version}') !!}----------------------------------------------------------->
-            <!--{!! Theme::css('vendor/adminlte/colors/skin-blue.min.css?t={cache-version}') !!}------------------------------------------------>
-            <!--{!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}---------------------------------------------------->
-            <!--{!! Theme::css('vendor/animate/animate.min.css?t={cache-version}') !!}---------------------------------------------------------->
-            <!-- Ability to customize Jexactyl theme ------------------------------------------------------------------------------------------->
-            <!--<link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/css/{{ config('theme.admin', 'jexactyl') }}.css">-->
-            <!-- Sistema Antigo de cores ------------------------------------------------------------------------------------------------------->
-
-            <!-- Novo Sistema de Cores ---------------------------------------------------------------------------------------------------->
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/select2/select2.min.css">
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/bootstrap/bootstrap.min.css">
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/adminlte/admin.min.css">
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/adminlte/colors/skin-blue.min.css">
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/sweetalert/sweetalert.min.css">
-            <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/vendor/animate/animate.min.css">
+            {!! Theme::css('vendor/select2/select2.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/bootstrap/bootstrap.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/admin.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/sidebar/sidebar.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/colors/skin-blue.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/animate/animate.min.css?t={cache-version}') !!}
             <link rel="stylesheet" href="/themes/{{ config('theme.admin', 'jexactyl') }}/css/{{ config('theme.admin', 'jexactyl') }}.css">
-            <!-- Novo Sistema de Cores ---------------------------------------------------------------------------------------------------->
+            <!-- Ability to customize Jexactyl theme -->
 
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-            
-        @show
-    </head>
+            @show
+        </head>
         <body class="hold-transition skin-blue fixed sidebar-mini">
             <div class="wrapper">
                 <header class="main-header">
@@ -58,74 +46,79 @@
                     </a>
                 </header>
                 <aside class="main-sidebar">
+                    <!-- Novo codigo-->
+                    <a style="margin-left: 20px; margin-top: 10px;">
+                    <img src="https://avatars.githubusercontent.com/u/91636558" width="48" height="48" />
+                                        <span class="transition-text" style="margin-left: 8px; font-weight: bold; font-size: 20px; color: #FFF;">{{ config('app.name', 'Jexactyl') }}</span>
+                                    </a>
+                            <!------------>
                     <section class="sidebar">
                         <ul class="sidebar-menu">
-                            <!-- Novo codigo-->
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.index') ?: 'desactive' }}">
-                                <a href="{{ route('index') }}">
+                                <a href="{{ route('index')}}">
                                     <i data-feather="home" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Voltar ao Painel</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Voltar ao Painel</span> 
                                 </a>
                             </li>
                         <!-- Novo codigo-->
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.index') ?: 'active' }}">
                                 <a href="{{ route('admin.index')}}">
                                     <i data-feather="tool" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Configurações</span> 
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Configurações</span> 
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.tickets') ?: 'active' }}">
                                 <a href="{{ route('admin.tickets.index')}}">
                                     <i class="fa fa-ticket" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Tickets</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Tickets</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.api') ?: 'active' }}">
                                 <a href="{{ route('admin.api.index')}}">
                                     <i data-feather="git-branch" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Aplicação de API</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Aplicação de API</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.databases') ?: 'active' }}">
                                 <a href="{{ route('admin.databases') }}">
                                     <i data-feather="database" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Banco de Dados</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Banco de Dados</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.locations') ?: 'active' }}">
                                 <a href="{{ route('admin.locations') }}">
                                     <i data-feather="navigation" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Localizações</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Localizações</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.nodes') ?: 'active' }}">
                                 <a href="{{ route('admin.nodes') }}">
                                     <i data-feather="layers" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Nodes</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Nodes</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.servers') ?: 'active' }}">
                                 <a href="{{ route('admin.servers') }}">
                                     <i data-feather="server" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Servidores</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Servidores</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.users') ?: 'active' }}">
                                 <a href="{{ route('admin.users') }}">
                                     <i data-feather="users" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Usuários</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Usuários</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.mounts') ?: 'active' }}">
                                 <a href="{{ route('admin.mounts') }}">
                                     <i data-feather="hard-drive" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Montagens</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Montagens</span>
                                 </a>
                             </li>
                             <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.nests') ?: 'active' }}">
                                 <a href="{{ route('admin.nests') }}">
                                     <i data-feather="archive" style="margin-left: 12px;"></i>
-                                    <span style="margin-left: 4px; font-size: 18px;">Nests</span>
+                                    <span style="margin-left: 4px; font-weight: bold; font-size: 18px;">Nests</span>
                                 </a>
                             </li>
                         </ul>

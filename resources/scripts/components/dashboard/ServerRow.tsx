a1bdedc5214fadf8dd2acc00b68a95cf8ac66746
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-13 21:33:23 -0400

    Update ServerRow.tsx

--- resources/scripts/components/dashboard/ServerRow.tsx
+++ resources/scripts/components/dashboard/ServerRow.tsx
@@ -110,7 +110,7 @@ export default ({ server, className }: { server: Server; className?: string }) =
                     isSuspended ? (
                         <div css={tw`flex-1 text-center`}>
                             <span css={tw`bg-red-500 rounded px-2 py-1 text-red-100 text-xs`}>
-                                {server.status === 'suspended' ? 'Suspended' : 'Erro de conexão'}
+                                {server.status === 'suspended' ? 'Suspenso' : 'Erro de conexão'}
                             </span>
                         </div>
                     ) : server.isTransferring || server.status ? (

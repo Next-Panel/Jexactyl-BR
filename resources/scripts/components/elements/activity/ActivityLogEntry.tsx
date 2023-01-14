d29bee61d1d625a081ed76fed717151e0d868ec2
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-13 21:35:32 -0400

    Update ActivityLogEntry.tsx

--- resources/scripts/components/elements/activity/ActivityLogEntry.tsx
+++ resources/scripts/components/elements/activity/ActivityLogEntry.tsx
@@ -53,8 +53,8 @@ export default ({ activity, children }: Props) => {
             <div className={'col-span-10 sm:col-span-9 flex'}>
                 <div className={'flex-1 px-4 sm:px-0'}>
                     <div className={'flex items-center text-gray-50'}>
-                        <Tooltip placement={'top'} content={actor?.email || 'System User'}>
-                            <span>{actor?.username || 'System'}</span>
+                        <Tooltip placement={'top'} content={actor?.email || 'Usuário do sistema'}>
+                            <span>{actor?.username || 'Sistema'}</span>
                         </Tooltip>
                         <span className={'text-gray-400'}>&nbsp;&mdash;&nbsp;</span>
                         <Link
@@ -65,12 +65,12 @@ export default ({ activity, children }: Props) => {
                         </Link>
                         <div className={classNames(style.icons, 'group-hover:text-gray-300')}>
                             {activity.isApi && (
-                                <Tooltip placement={'top'} content={'Using API Key'}>
+                                <Tooltip placement={'top'} content={'Usando chave de API'}>
                                     <Icon.Key />
                                 </Tooltip>
                             )}
                             {activity.event.startsWith('server:sftp.') && (
-                                <Tooltip placement={'top'} content={'Using SFTP'}>
+                                <Tooltip placement={'top'} content={'Usando SFTP'}>
                                     <Icon.Folder />
                                 </Tooltip>
                             )}

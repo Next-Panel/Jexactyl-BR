a9241f1c6ce63062c9ed4b609c3501ec4df285a6
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-02-15 11:58:02 -0400

    10/11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    Jexactyl\Http\Kernel::class
+    Pterodactyl\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    Jexactyl\Console\Kernel::class
+    Pterodactyl\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    Jexactyl\Exceptions\Handler::class
+    Pterodactyl\Exceptions\Handler::class
 );
 
 /*

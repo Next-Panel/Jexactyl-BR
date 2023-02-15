ac7670746b8a5f44e471188b0917410490f26997
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-02-15 11:13:36 -0400

    5/11

--- routes/api-application.php
+++ routes/api-application.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Jexactyl\Http\Controllers\Api\Application;
+use Pterodactyl\Http\Controllers\Api\Application;
 
 /*
 |--------------------------------------------------------------------------

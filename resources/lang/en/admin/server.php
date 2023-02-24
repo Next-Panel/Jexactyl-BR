0e2d9f37063b3bd23b5d4a38d2423891399e0176
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-02-24 17:24:41 -0400

    Criar pasta En
    isso evitará possíveis problemas

---
+++ resources/lang/en/admin/server.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+
+return [
+    'exceptions' => [
+        'no_new_default_allocation' => 'Você está tentando excluir a alocação padrão para este servidor, mas não há alocação de recuo para usar.',
+        'marked_as_failed' => 'Este servidor foi marcado como tendo falhado em uma instalação anterior. O status atual não pode ser alternado neste estado.',
+        'bad_variable' => 'Houve um erro de validação com a variável :name.',
+        'daemon_exception' => 'Houve uma exceção ao tentar se comunicar com o daemon resultando em um código de resposta HTTP/: Esta exceção foi registrada. (request id: :request_id)',
+        'default_allocation_not_found' => 'A alocação padrão solicitada não foi encontrada nas alocações deste servidor.',
+    ],
+    'alerts' => [
+        'startup_changed' => 'A configuração de inicialização para este servidor foi atualizada. Se o Nests ou o Egg deste servidor for alterado, uma reinstalação estará ocorrendo agora.',
+        'server_deleted' => 'O servidor foi excluído com sucesso do sistema.',
+        'server_created' => 'O servidor foi criado com sucesso no painel. Por favor, permita que o daemon se instale e em alguns minutos inicie este servidor.',
+        'build_updated' => 'Os detalhes da compilação deste servidor foram atualizados. Algumas alterações podem exigir um reiniciar para fazer efeito.',
+        'suspension_toggled' => 'O status de suspensão do servidor foi alterado para :status.',
+        'rebuild_on_boot' => 'Este servidor foi marcado como exigindo uma reconstrução do Contêiner Docker. Isso acontecerá na próxima vez que o servidor for iniciado.',
+        'install_toggled' => 'O status de instalação deste servidor foi alternado.',
+        'server_reinstalled' => 'Este servidor foi enfileiado para uma reinstalação a partir de agora.',
+        'details_updated' => 'Os detalhes do servidor foram atualizados com sucesso.',
+        'docker_image_updated' => 'Alterou com sucesso a imagem padrão do Docker para usar para este servidor. Uma reinicialização é necessária para aplicar essa alteração.',
+        'node_required' => 'Você deve ter pelo menos um Node configurado antes de poder adicionar um servidor a este painel.',
+        'transfer_nodes_required' => 'Você deve ter pelo menos dois Nodes configurados antes de poder transferir servidores.',
+        'transfer_started' => 'A transferência do servidor foi iniciada.',
+        'transfer_not_viable' => 'O Node selecionado não tem o espaço ou memória de disco necessários para acomodar este servidor.',
+    ],
+];

c1c6e638093d4bc2445b7de859445861f509c00e
Drylian <109999325+drylian@users.noreply.github.com>
2023-05-04 00:08:02 -0300

    database MercadoPago

---
+++ database/migrations/2023_05_03_011716_create_mercado_pago_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mercado_pago', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mercado_pago');
+    }
+};
